848c89a617a87f5715988a098b69b6e0255c643c
2010002 <k.arihara@jaist.ac.jp>
2021-09-10 21:42:16 +0900

    [fix]adjacents list
    [TODO] implementation of  postprocess func  in refinemts func

--- RidgeRefine2D.cs
+++ RidgeRefine2D.cs
@@ -4,11 +4,13 @@ using System.Numerics;
 
 namespace Arihara.GuideSmoke
 {
-  struct AdjacencyList
+  struct Pixel
   {
+    public bool isInvalid;
     public int ix, iy;
-    public List<AdjacencyList> lists;
+    public List<Pixel> adjacents;
     public Vector2 pos;
+    public float dv;
   }
 
   /*
@@ -21,7 +23,7 @@ namespace Arihara.GuideSmoke
     float[,] ftle;
     Vector2[,] positions;
     Vector2[,] gradients;
-    List<AdjacencyList> headLists;
+    Pixel[,] pixels;
     int lenX, lenY;
     float deltaX, deltaY;
 
@@ -29,6 +31,8 @@ namespace Arihara.GuideSmoke
     float delta = 0.5f;
     float sigma = 0.25f;
     float omega = 0.05f;
+    float d_max;
+    int k_cut = 5;
     #endregion
 
 
@@ -43,42 +47,60 @@ namespace Arihara.GuideSmoke
       this.deltaY = dy;
     }
 
-    public void SetParameters(float d, float s, float o)
+    public void SetParameters(float d, float s, float o, float d_m, int k)
     {
       this.delta = d;
       this.sigma = s;
       this.omega = o;
+      this.d_max = d_m;
+      this.k_cut = k;
     }
 
     public void SubPixelRidgeRefinement()
     {
       ConstructAdjList();
       ComputeFTLEGradient();
+      Refinements();
     }
 
     private void Refinements()
     {
       Dictionary<(int, int), Vector2> new_positions = new Dictionary<(int, int), Vector2>();
-      foreach (var adj in headLists)
+      for (int ix = 0; ix < lenX; ix++)
       {
-        if (adj.lists.Count == 1)
+        for (int iy = 0; iy < lenY; iy++)
         {
-          int ix = adj.ix;
-          int iy = adj.iy;
-          Vector2 nv = UnitLengthPerpendicular2D(adj.pos);
-          Vector2 g = gradients[ix, iy];
-          Vector2 rv = Vector2.Dot(nv, g) * nv;
-          Vector2 p = positions[ix, iy];
-          new_positions.Add((ix, iy), p + rv * this.delta);
-        }
-        else
-        {
-        }
+          Pixel you = pixels[ix, iy];
+          if(you.isInvalid) continue;
+          if (you.adjacents.Count == 1)
+          {
+            Pixel adjacent = you.adjacents[0];
+            Vector2 nv = UnitLengthPerpendicular2D(adjacent.pos - you.pos);
+            Vector2 g = gradients[adjacent.ix, adjacent.iy];
+            Vector2 rv = Vector2.Dot(nv, g) * nv;
+            Vector2 new_pv = you.pos + this.delta * rv;
+            new_positions.Add((ix, iy), new_pv);
+          }
+          else
+          {
+            Vector2 rv = Vector2.Zero;
+            Vector2 pu = Vector2.Zero;
+            foreach (Pixel adjacent in you.adjacents)
+            {
+              Vector2 nv = UnitLengthPerpendicular2D(adjacent.pos - you.pos);
+              Vector2 g = gradients[adjacent.ix, adjacent.iy];
+              rv += Vector2.Dot(nv, g) * nv;
+              pu += adjacent.pos;
+            }
+            Vector2 new_pv = (1 - this.sigma) * you.pos + this.sigma * pu / you.adjacents.Count + this.delta * rv;
+            new_positions.Add((ix, iy), new_pv);
+          }
 
-        foreach (var np in new_positions)
-        {
-          (int ix, int iy) index = np.Key;
-          positions[index.ix, index.iy] = np.Value;
+          foreach (var np in new_positions)
+          {
+            (int ix, int iy) index = np.Key;
+            positions[index.ix, index.iy] = np.Value;
+          }
         }
       }
 
@@ -89,6 +111,11 @@ namespace Arihara.GuideSmoke
         float k2 = (float)Math.Sqrt((a * a) / (a * a + b * b));
         float k1 = -b * k2 / a;
         return new Vector2(k1, k2);
+      }
+
+      void PostProcess()
+      {
+
       }
     }
 
@@ -114,40 +141,43 @@ namespace Arihara.GuideSmoke
 
     private void ConstructAdjList()
     {
-      bool[,] isAddedGraph = new bool[lenX, lenY];
-      headLists = new List<AdjacencyList>();
+      bool[,] isAddedPixels = new bool[lenX, lenY];
+      pixels = new Pixel[lenX, lenY];
       for (int ix = 0; ix < lenX; ix++)
       {
         for (int iy = 0; iy < lenY; iy++)
         {
-          AdjacencyList head = new AdjacencyList();
-          headLists.Add(head);
-          SetValue(ref head, ix, iy);
+          if (region[ix, iy] && !isAddedPixels[ix, iy]) pixels[ix, iy] = new Pixel();
+          SetValue(ref pixels[ix, iy], ix, iy);
         }
       }
 
-      void SetValue(ref AdjacencyList al, int ix, int iy)
+      void SetValue(ref Pixel px, int ix, int iy)
       {
         bool N = false;
         bool W = false;
         bool S = false;
         bool E = false;
         Vector2 p = positions[ix, iy];
-        al.pos = new Vector2(p.X, p.Y);
-        al.lists = new List<AdjacencyList>();
-        al.ix = ix;
-        al.iy = iy;
-
-        isAddedGraph[ix, iy] = true;
+        px.pos = new Vector2(p.X, p.Y);
+        px.adjacents = new List<Pixel>();
+        px.isInvalid = false;
+        px.ix = ix;
+        px.iy = iy;
+        px.dv = 0;
+        isAddedPixels[ix, iy] = true;
 
         /* West */
         if (0 < ix)
         {
-          if (region[ix - 1, iy] && !isAddedGraph[ix - 1, iy])
+          if (region[ix - 1, iy])
           {
-            AdjacencyList w = new AdjacencyList();
-            al.lists.Add(w);
-            SetValue(ref w, ix - 1, iy);
+            if (!isAddedPixels[ix - 1, iy])
+            {
+              pixels[ix - 1, iy] = new Pixel();
+              isAddedPixels[ix - 1, iy] = true;
+            }
+            px.adjacents.Add(pixels[ix - 1, iy]);
             W = true;
           }
         }
@@ -155,11 +185,14 @@ namespace Arihara.GuideSmoke
         /* East */
         if (ix < lenX - 1)
         {
-          if (region[ix + 1, iy] && !isAddedGraph[ix + 1, iy])
+          if (region[ix + 1, iy])
           {
-            AdjacencyList e = new AdjacencyList();
-            al.lists.Add(e);
-            SetValue(ref e, ix + 1, iy);
+            if (!isAddedPixels[ix + 1, iy])
+            {
+              pixels[ix + 1, iy] = new Pixel();
+              isAddedPixels[ix + 1, iy] = true;
+            }
+            px.adjacents.Add(pixels[ix + 1, iy]);
             E = true;
           }
         }
@@ -167,11 +200,14 @@ namespace Arihara.GuideSmoke
         /* North */
         if (0 < iy)
         {
-          if (region[ix, iy - 1] && !isAddedGraph[ix, iy - 1])
+          if (region[ix, iy - 1])
           {
-            AdjacencyList n = new AdjacencyList();
-            al.lists.Add(n);
-            SetValue(ref n, ix, iy - 1);
+            if (!isAddedPixels[ix, iy - 1])
+            {
+              pixels[ix, iy - 1] = new Pixel();
+              isAddedPixels[ix, iy - 1] = true;
+            }
+            px.adjacents.Add(pixels[ix, iy - 1]);
             N = true;
           }
         }
@@ -179,11 +215,14 @@ namespace Arihara.GuideSmoke
         /* South */
         if (iy < lenY - 1)
         {
-          if (region[ix, iy + 1] && !isAddedGraph[ix, iy + 1])
+          if (region[ix, iy + 1])
           {
-            AdjacencyList s = new AdjacencyList();
-            al.lists.Add(s);
-            SetValue(ref s, ix, iy + 1);
+            if (!isAddedPixels[ix, iy + 1])
+            {
+              pixels[ix, iy + 1] = new Pixel();
+              isAddedPixels[ix, iy + 1] = true;
+            }
+            px.adjacents.Add(pixels[ix, iy + 1]);
             S = true;
           }
         }
@@ -191,44 +230,56 @@ namespace Arihara.GuideSmoke
         /* NW */
         if (!(N || W) && 0 < iy && 0 < ix)
         {
-          if (region[ix - 1, iy - 1] && !isAddedGraph[ix - 1, iy - 1])
+          if (region[ix - 1, iy - 1])
           {
-            AdjacencyList nw = new AdjacencyList();
-            al.lists.Add(nw);
-            SetValue(ref nw, ix - 1, iy - 1);
+            if (!isAddedPixels[ix - 1, iy - 1])
+            {
+              pixels[ix - 1, iy - 1] = new Pixel();
+              isAddedPixels[ix - 1, iy - 1] = true;
+            }
+            px.adjacents.Add(pixels[ix - 1, iy - 1]);
           }
         }
 
         /* NE */
         if (!(N || E) && 0 < iy && ix < lenX - 1)
         {
-          if (region[ix + 1, iy - 1] && !isAddedGraph[ix + 1, iy - 1])
+          if (region[ix + 1, iy - 1])
           {
-            AdjacencyList ne = new AdjacencyList();
-            al.lists.Add(ne);
-            SetValue(ref ne, ix + 1, iy - 1);
+            if (!isAddedPixels[ix + 1, iy - 1])
+            {
+              pixels[ix + 1, iy - 1] = new Pixel();
+              isAddedPixels[ix + 1, iy - 1] = true;
+            }
+            px.adjacents.Add(pixels[ix + 1, iy - 1]);
           }
         }
 
         /* SW */
         if (!(S || W) && iy < lenY - 1 && 0 < ix)
         {
-          if (region[ix - 1, iy + 1] && !isAddedGraph[ix - 1, iy + 1])
+          if (region[ix - 1, iy + 1])
           {
-            AdjacencyList sw = new AdjacencyList();
-            al.lists.Add(sw);
-            SetValue(ref sw, ix - 1, iy + 1);
+            if (!isAddedPixels[ix - 1, iy + 1])
+            {
+              pixels[ix - 1, iy + 1] = new Pixel();
+              isAddedPixels[ix - 1, iy + 1] = true;
+            }
+            px.adjacents.Add(pixels[ix - 1, iy + 1]);
           }
         }
 
         /* SE */
         if (!(S || E) && iy < lenY - 1 && ix < lenX - 1)
         {
-          if (region[ix + 1, iy + 1] && !isAddedGraph[ix + 1, iy + 1])
+          if (region[ix + 1, iy + 1])
           {
-            AdjacencyList se = new AdjacencyList();
-            al.lists.Add(se);
-            SetValue(ref se, ix + 1, iy + 1);
+            if (!isAddedPixels[ix + 1, iy + 1])
+            {
+              pixels[ix + 1, iy + 1] = new Pixel();
+              isAddedPixels[ix + 1, iy + 1] = true;
+            }
+            px.adjacents.Add(pixels[ix + 1, iy + 1]);
           }
         }
       }
